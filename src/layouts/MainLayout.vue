1cedec4eb657c7977087807ae5451a9edfba334c
Dolpheyn <farissufyan99@gmail.com>
2021-05-16 14:41:24 +0800

    init

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,57 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-white text-black">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="left = !left" />
+
+        <q-toolbar-title>
+          <span class="bank-gothic">
+            Ad Lunam - To the Moon 🌑
+          </span>
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="left" side="left" bordered>
+      <!-- drawer content -->
+      <ul>
+        <li>Introduction</li>
+        <li>History</li>
+        <li>Reusable Rockets</li>
+        <ul>
+          <li>Falcon 1</li>
+          <li>Falcon 9</li>
+          <li>Falcon Heavy</li>
+          <li>Dragon</li>
+        </ul>
+        <li>Starship</li>
+        <li>Starlink</li>
+      </ul>
+      <ul>
+        <li>link</li>
+        <li>to</li>
+        <li>SpaceX</li>
+        <li>website,</li>
+        <li>youtube,</li>
+        <li>twitter</li>
+      </ul>
+    </q-drawer>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      left: false,
+      right: false
+    }
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,146 @@
+<template>
+  <q-page class="page">
+    <h1 class="text-h2">Ad Lunam 🌑</h1>
+    <span class="text-h5">
+      The story of the human civilization going to the moon–again, this time
+      powered by low cost, reusable rockets.
+    </span>
+    <div class="text-body1 q-mt-xl">
+
+      <p>
+        Our 🌟space🌟 story started in the 20th century, during the Cold War.
+        The Soviets launched the first ever man-made object into orbit, the
+        Sputnik 1, after advancements in space technology made possible by the
+        WWII.  This event triggered what we know today as the Space Race.
+      </p>
+
+      <p>
+        NASA was born short after. For the next years, two giants–the US and the
+        Soviets–made an international space show by dumping their money to prove
+        to the world who has the better technology. The space programs were
+        televised to show off their greatness to the world, simultaneously
+        making their rival pissed off. For almost a decade later, we saw the
+        giants furiously sending men to space, ending with the US sending Neil
+        Armstrong to the moon in July of 1969. This continued to the year 1972,
+        an by then we have sent a total of 12 humans to walk on the moon.
+      </p>
+
+      <p>
+        Humans got super excited. We were expecting to push our boundaries far
+        beyond what we were capable of. It was that same feeling of discovering
+        again, like when we first sailed across the ocean. However, for the next
+        49 years, the people-on-the-moon count is stil...12, and we don't have
+        anything new planned, at all. The Space Race was just two giants being
+        cocky during a war crisis after all. After it ended with one's satisfied
+        ego, and the other's shame, the two nations don't find it worth to spend
+        their fortunes to go beyond the orbit anymore.
+      </p>
+
+      <p>
+        Fortunately, that was the case until <span class="bank-gothic text-h6">SpaceX</span>
+        shows up.
+      </p>
+
+      <h2>History of <span class="bank-gothic">SpaceX</span></h2>
+
+      <p>
+        <span class="bank-gothic text-h6">SpaceX</span>
+        <span class="text-strike">
+          manufactures and launches the world’s most advanced
+        </span>
+
+        makes and launches cool and badass rockets . The company was founded in
+        2002 by Elon Musk to revolutionize space transportation,
+
+        <span class="text-bold">
+          with the ultimate goal of making life multiplanetary.
+        </span>
+        Musk is famously known for founding Tesla Motors, X.com (later known as
+        PayPal) and his first multimillionaire company, Zip2.
+      </p>
+
+      <p>
+        Like any other successful businesses Musk had founded, SpaceX is no
+        different.  It became the first privately funded group to put a payload
+        in Earth orbit in 2008. Since then, the company has continued to
+        impress, launching unmanned cargo vehicles to the International Space
+        Station (ISS) and winning a contract from NASA to fly astronauts as
+        early as 2017. <span class="text-bold">One of SpaceX’s most notable
+        history-making moments was when its first stage rocket—the Falcon
+        9🚀—became the first one to launch, then autonomously landed upright
+        on its feet.</span> Now in 2021, watching SpaceX landing its rockets
+        is already a routine.
+      </p>
+
+      <p>
+        SpaceX makes their rockets from scratch. They keep the outsourcing cost low by
+        building roughly 80% from the parts of a rocket in their own factory. In one end
+        of their factory comes the raw materials, in the other end, rockets. One study
+        by NASA and the Air Force estimated that the price tag of the process, from
+        designing to the launching of the Falcon 9 rocket, was about a third of what it
+        would have cost NASA.
+      </p>
+
+      <p>
+        To reach Musk’s ultimate goal, he figured space travelling costs should be
+        lowered, by a lot. So, he made his rockets reusable. After recovery from
+        landing, the rockets must be able to be refurbished and reflown in minimal
+        time and with minimal cost. Only then can they be reflown, reducing launch
+        costs significantly. SpaceX have been successfully doing this. Until May
+        of 2021, SpaceX’s reused flights count is at 61, with the booster with the
+        highest flight count being the B1051, sitting at 9 launches.
+      </p>
+
+      <p>
+        In April 2020, SpaceX made yet another first in history by flying 4
+        astronauts in the Crew-2 mission with NASA with reused, or termed by NASA,
+        “flight proven” rocket. SpaceX's Falcon 9 booster that launched the Crew-1
+        mission in November 2020 is brought to port after landing on an automatic
+        drone ship at sea. This booster is reused for the Crew-2 mission, being
+        the first rocket booster that has ever been used for multiple human
+        launches.
+      </p>
+
+      <p>
+        As of 2021, SpaceX is actively developing the Starship system, with the
+        intent to make it a fully-reusable two-stage launch vehicle, intended to
+        replace all of its existing launch vehicles and spacecraft used for
+        satellite delivery and human transport—Falcon 9, Falcon Heavy and
+        Dragon—and also eventually support flights to the Moon and Mars. In
+        addition it could be used for point-to-point transportation on Earth.
+      </p>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
+
+<style>
+.page {
+  margin: 50px auto;
+  padding: 0 40px;
+  word-wrap: break-word;
+}
+
+@media (min-width: 576px) {
+  .page {
+      max-width: 540px;
+  }
+}
+
+@media (min-width: 786px) {
+  .page {
+      max-width: 720px;
+  }
+}
+
+@media (min-width: 1280px) {
+  .page {
+      max-width: 1100px;
+  }
+}
+</style>
