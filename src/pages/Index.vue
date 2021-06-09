e639ad794f6457e244f4a06343cbfe59f23ded2d
Dolpheyn <farissufyan99@gmail.com>
2021-06-09 14:46:39 +0800

    Edit paragraphs
    Fix typos and reword

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,7 +2,7 @@
   <q-page class="page">
     <h1 class="text-h1">Ad Lunam 🌑</h1>
     <span class="text-h5">
-      Can the human civilization going to the moon–again, this time
+      Can the human civilization go to the moon–again, this time
       powered by low cost, reusable rockets?
     </span>
     <div class="text-body1 q-mt-xl">
@@ -10,10 +10,11 @@
       <h2>The Human and Space Chronicle</h2>
 
       <p>
-        Our space story started in the 20th century, during the Cold War.
-        The Soviets launched the first ever man-made object into orbit, the
-        Sputnik 1, after advancements in space technology made possible by the
-        WWII.  This event triggered what we know today as the Space Race.
+        First, a recap. Our space story started in the 20th century, during the
+        Cold War.  The Soviets launched the first ever man-made object into
+        orbit, the Sputnik 1, after advancements in space technology made
+        possible by the WWII.  This event triggered what we know today as the
+        Space Race.
       </p>
 
       <p>
@@ -49,7 +50,7 @@
         <span class="bank-gothic text-h6">SpaceX</span>
         <span class="text-strike">
           manufactures and launches the world’s most advanced
-        </span> makes and launches
+        </span> builds and launches
         <span class="text-italic"> cool and badass rockets</span>. The company
         was founded in 2002 by Elon Musk to revolutionize space transportation,
 
@@ -89,11 +90,11 @@
 
       <h2>Reusable Rockets 🚀</h2>
 
-      <figure>
+      <figure class="q-mx-none">
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/0/0e/Falcon9_rocket_family.svg"
           alt="Falcon 9 rocket family"
-          style="border: 5px solid #555; width: 100%;"
+          style="border: 1px solid #555; width: 100%;"
         >
         <em>
         <figcaption>The Falcon 9 rocket family | image: wikimedia</figcaption>
@@ -124,11 +125,11 @@
       </p>
 
       <p>
-        So, boosters are the first stage of the two-stage rocket launch vehicle
+        Boosters are the first stage of the two-stage rocket launch vehicle
         that SpaceX builds. Boosters burns rocket fuels at liftoff, up the rocket
         goes, it accelerates the rocket, and get detached at some point before
         continuing to orbit on its own path. Of course, for SpaceX's boosters
-        they then land autonomously on a groud landing pad or a drone landing
+        they then land autonomously on a ground landing pad or a drone landing
         ship at the sea.
       </p>
 
@@ -145,7 +146,7 @@
       <q-card class="q-pa-md q-my-md"><BoosterLaunchesBarChart/></q-card>
       <q-card class="q-pa-md q-my-md"><BoosterLaunchesPieChart/></q-card>
 
-      <p>
+      <p class="q-mt-xl">
         In April 2020, SpaceX made yet another first in history by flying 4
         astronauts in the Crew-2 mission with NASA with reused, or termed by
         NASA, “flight proven” rocket. SpaceX's Falcon 9 booster that launched
@@ -163,7 +164,7 @@
         surveillance, and scientific satellites,
         <span><a href="https://en.wikipedia.org/wiki/Commercial_Resupply_Services">
         sending cargo and supplies to the ISS</a></span>, and more. Let's explore who
-        have been using SpaceX's reusable rokets that are provided by their
+        have been trusting SpaceX, using their reusable rokets that are provided by their
         services.
       </p>
 
@@ -187,7 +188,7 @@
 
       <h2>The Starship</h2>
 
-      <figure>
+      <figure class="q-mx-none">
         <img
           src="https://imgix.bustle.com/uploads/image/2021/4/19/1b2d4014-5a46-403f-a494-b18759fd8a29-for_press_release.jpg"
           alt="Illustration of SpaceX's lunar lander on the moon."
